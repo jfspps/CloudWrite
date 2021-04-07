4f8010dc76a14dc194fc32991899ce20c019a5cb
jfspps <jfsapps@gmail.com>
2021-04-07 16:53:49 +0100

    added more fields to ExpositionDTO test

--- src/test/java/com/example/cloudwrite/controller/api/ExpositionDTOControllerTest.java
+++ src/test/java/com/example/cloudwrite/controller/api/ExpositionDTOControllerTest.java
@@ -2,6 +2,11 @@ package com.example.cloudwrite.controller.api;
 
 import com.example.cloudwrite.JAXBModel.ExpositionPieceDTO;
 import com.example.cloudwrite.JAXBModel.ExpositionPieceDTOList;
+import com.example.cloudwrite.api.mapper.ExpositionPieceMapper;
+import com.example.cloudwrite.api.mapper.StandfirstMapper;
+import com.example.cloudwrite.model.Citation;
+import com.example.cloudwrite.model.KeyResult;
+import com.example.cloudwrite.model.Standfirst;
 import com.example.cloudwrite.service.DTO.ExpositionPieceDTOService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -25,7 +31,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(MockitoExtension.class)
 class ExpositionDTOControllerTest {
 
-    private final String KEYWORD = "theTests";
+    private final String TITLE = "a title";
+    private final String KEYWORD = "a keyword";
+    private final String PURPOSE = "a purpose";
+    private final String PROGRESS = "current progress";
+    private final String FUTUREWORK = "what's next";
+
+    private final String STANDFIRSTrationale = "why this is relevant";
+    private final String STANDFIRSTapproach = "how to handle it";
+
+    private final String REF = "a reference";
+    private final String RESULTDESC = "a result";
+
+    private Standfirst standfirst;
+    private Citation citation;
+    private KeyResult keyResult;
 
     @Mock
     ExpositionPieceDTOService expositionPieceDTOService;
@@ -45,8 +65,19 @@ class ExpositionDTOControllerTest {
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(expositionDTOController).build();
 
+        standfirst = Standfirst.builder().id(1L).rationale(STANDFIRSTrationale).approach(STANDFIRSTapproach).build();
+        citation = Citation.builder().id(2L).reference(REF).build();
+        keyResult = KeyResult.builder().description(RESULTDESC).id(3L).priority(3).build();
+
         expositionPieceDTO1.setId(1L);
         expositionPieceDTO1.setKeyword(KEYWORD);
+        expositionPieceDTO1.setTitle(TITLE);
+        expositionPieceDTO1.setStandfirstDTO(StandfirstMapper.INSTANCE.standfirstToStandfirstDTO(standfirst));
+        expositionPieceDTO1.setExpositionPurpose(PURPOSE);
+        expositionPieceDTO1.setCurrentProgress(PROGRESS);
+        expositionPieceDTO1.setKeyResultDTOList(ExpositionPieceMapper.toKeyResultDTOList(List.of(keyResult)));
+        expositionPieceDTO1.setCitationDTOList(ExpositionPieceMapper.toCitationDTOList(List.of(citation)));
+        expositionPieceDTO1.setFutureWork(FUTUREWORK);
 
         expositionPieceDTO2.setId(2L);
         expositionPieceDTO2.setKeyword(KEYWORD + "sss");
@@ -76,5 +107,6 @@ class ExpositionDTOControllerTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.expositionPiece", hasSize(2)));
+
     }
 }
